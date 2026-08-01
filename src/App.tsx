a876fc3a83583238b06c7e747a79339251e05188
Pedro Matos <pedroheunapolis@gmail.com>
2026-08-01 01:35:23 -0300

    style: add header and css variables

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,20 @@ function App() {
 
   return (
     <>
+      <header className='header'>
+        <p className='eyebrow'>Ministério de Jovens IBNG</p>
+        <h1 className='header-title'>
+          Manifeste
+          <br />
+          <span className='title-emphasis'>o Reino</span>
+        </h1>
+        <div className='year'>2026</div>
+
+        <a href="#" className='order'>Fazer pedido</a>
+
+        <div className='mouse-icon'></div>
+
+      </header>
 
     </>
   )
