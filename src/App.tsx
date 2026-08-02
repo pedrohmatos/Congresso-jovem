16780aaaf48886bcbfa0b7b3df752773b308aa71
Pedro Matos <pedroheunapolis@gmail.com>
2026-08-02 02:00:28 -0300

    style: add t-shirt section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import blueTshirt from './assets/azul-marinho.jpeg';
 
 function App() {
 
@@ -18,7 +19,27 @@ function App() {
         <div className='mouse-icon'></div>
 
       </header>
-
+      <main className='main'>
+        <section className='first-section'>
+          <div>
+            <div className='first-section-titles'>
+              <p className='eyebrow-2'>Coleção 2026</p>
+              <h2 className='first-section-title'>Modelos oficiais</h2>
+            </div>
+            <figure>
+              <img src={blueTshirt} alt="blue-shirt" className='tshirt' />
+              <figcaption className='tshirt-caption-container'>
+                <button type='button' className='tshirt-caption'>
+                  Azul marinho
+                </button>
+                <button type='button' className='tshirt-caption'>
+                  Mostarda
+                </button>
+              </figcaption>
+            </figure>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
